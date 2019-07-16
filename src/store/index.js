3f70099da39c1f775212f8b96fe3dd7e4cff81e5
Andrey Lashenko <andreylashenko@mail.ru>
2019-07-16 21:18:22 +0300

    refactor project

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,6 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    contacts: [],
     recordings: [],
     recordsCount : 0,
     todayRecordsCount : 0,
@@ -14,28 +13,6 @@ export default new Vuex.Store({
   mutations:{
     setRecordings(state, payload) {
       state.recordings = payload;
-    },
-    addPhone(state, payload) {
-      const contact = state.contacts.findIndex((el => el.id == payload.contactId))
-      state.contacts[contact].phones.push(payload.phone)
-    },
-    setContacts(state, payload) {
-      state.contacts = payload
-    },
-    editContact(state, payload) {
-      const updatedContact = state.contacts.findIndex((el => el.id == payload.id));
-      state.contacts[updatedContact].name = payload.name;
-      state.contacts[updatedContact].phones = payload.phones;
-    },
-    deleteContact(state, payload) {
-      state.contacts = this.state.contacts.filter(el => { return el.id != payload.id; });
-    },
-    createContact(state, payload) {
-      if(state.contacts) {
-        state.contacts.push(payload)
-      } else {
-        state.contacts = [payload]
-      }
     },
     setRecordsCount(state, payload) {
       state.recordsCount = payload;
@@ -88,54 +65,6 @@ export default new Vuex.Store({
             return totalDuration
           })
     },
-    savePhone(context, payload) {
-      Vue.http.post('http://framework/contact/addPhone',
-        payload
-      ).then(response => {
-        context.commit('addPhone', payload)
-        return response
-      })
-    },
-    updateContact(context, payload) {
-      Vue.http.put('http://framework/contact/update',
-        payload
-      ).then(response => {
-         return response.json()
-      })
-       .then(contacts => {
-         context.commit('editContact', contacts["data"])
-         return contacts
-      })
-    },
-    loadContacts(context) {
-      Vue.http.get('http://framework/contact/list')
-        .then(response => {
-          return response.json()
-        })
-        .then(contacts => {
-          context.commit('setContacts', contacts["data"])
-          return contacts
-        })
-    },
-    deleteContact(context, payload) {
-      Vue.http.delete('http://framework/contact/delete',
-        payload
-      ).then(response => {
-        context.commit('deleteContact', payload.body)
-        return response
-      })
-    },
-    createContact(context, payload) {
-      Vue.http.post('http://framework/contact/create',
-        payload
-      ).then(response => {
-        return response.json()
-      })
-      .then(contacts => {
-        context.commit('createContact', contacts["data"])
-        return contacts
-      })
-    },
     findRecord(context, payload) {
       Vue.http.post('http://localhost:8081/leadzvon/list',
           payload
@@ -149,9 +78,6 @@ export default new Vuex.Store({
     }
   },
   getters: {
-    getContactList(state) {
-      return state.contacts;
-    },
     getRecordList(state) {
       return state.recordings;
     },
